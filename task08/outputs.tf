3c901a312b5ceb47ec271388a69b99ede424ecea
TigranAHarutyunyan <tigran.harutunyan.02@gmail.com>
2026-06-27 14:12:36 +0400

    Task 8 id is correct 11

--- task08/outputs.tf
+++ task08/outputs.tf
@@ -26,3 +26,8 @@ output "aci_fqdn" {
   value       = module.aci.aci_fqdn
   description = "FQDN of App in Azure Container Instance"
 }
+
+output "aks_lb_ip" {
+  value       = module.aks.aks_lb_ip
+  description = "IP of the Load Balancer"
+}
